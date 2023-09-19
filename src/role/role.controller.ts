e7e59d0eaa11ad3577ab55f66cf477ba8dbe70a8
夏思勇 <xiasiyong@ones.cn>
2023-09-19 10:36:28 +0800

    feat:role接口增加权限校验

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -6,35 +6,46 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { RoleGuard } from 'src/auth/guards/role.guard';
+import { Roles } from 'src/decorators/role.decorators';
+import { RoleEnum } from 'src/enum/role.enum';
 
+@UseGuards(JwtGuard, RoleGuard)
 @Controller('role')
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
+  @Roles(RoleEnum.SUPER_ADMIN)
   @Post()
   create(@Body() createRoleDto: CreateRoleDto) {
     return this.roleService.create(createRoleDto);
   }
 
+  @Roles(RoleEnum.SUPER_ADMIN)
   @Get()
   findAll() {
     return this.roleService.findAll();
   }
 
+  @Roles(RoleEnum.SUPER_ADMIN)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.roleService.findOne(+id);
   }
 
+  @Roles(RoleEnum.SUPER_ADMIN)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
     return this.roleService.update(+id, updateRoleDto);
   }
 
+  @Roles(RoleEnum.SUPER_ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.roleService.remove(+id);
